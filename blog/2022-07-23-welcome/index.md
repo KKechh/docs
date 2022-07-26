dd3d7a52ab17e4d9515adfb2b7f00d710e0c36bf
Xiaohui Lam <xiaohui.lam@e.hexdata.cn>
2022-07-27 00:01:51 +0800

    fixbug

---
+++ blog/2022-07-23-welcome/index.md
@@ -0,0 +1,12 @@
+---
+slug: Hello-HiCA
+title:  Hello world, Hello HiCA！
+authors: [sue]
+tags: [HiCA, ACME, 免费证书, 免费SSL, 免费通配符SSL]
+---
+
+![Docusaurus Plushie](./docusaurus-plushie-banner.jpeg)
+
+天空一声巨响，HiCA 闪亮登场。2022 年 7 月 22 日，HiCA横空出世，当日申请订单超过1200次，成功签发800+证书！
+
+我们的目标是，在 SSL 数字证书这件事上，消灭一切 UI！
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,13 +36,13 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/master/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/HiCA-ACME/docs/tree/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/master/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/HiCA-ACME/docs/tree/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
